8fada938222b537f04481a157f0d6ec0430999f4
RunsetTech <52034765+RunsetTech@users.noreply.github.com>
2020-08-23 13:39:41 +0430

    Update bottom.sh

--- bottom.sh
+++ bottom.sh
@@ -15,7 +15,7 @@ exit
 IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 export SERVER_HOST=$IP
 wget https://raw.githubusercontent.com/RunsetTech/wireguard-install/master/wireguard-install.sh -O wireguard-install.sh
-bash wireguard-install.sh
+yes | bash wireguard-install.sh
 git clone https://github.com/EchoEkhi/lead-knight
 cd lead-knight
 npm install
