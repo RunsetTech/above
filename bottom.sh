5c9b4f90d3807474296895ef8b66a30ddab8c06f
RunsetTech <52034765+RunsetTech@users.noreply.github.com>
2020-08-23 13:26:12 +0430

    Create bottom.sh

---
+++ bottom.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt install -y nodejs
+sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install npm
+sudo apt install -y mongodb
+use leadknight2
+db.leadknigh2t.insert({
+  name: "Chaitanya",
+  age: 30,
+  website: "beginnersbook.com"
+})
+IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+export SERVER_HOST=$IP
+wget https://raw.githubusercontent.com/l-n-s/wireguard-install/master/wireguard-install.sh -O wireguard-install.sh
+bash wireguard-install.sh
+git clone https://github.com/EchoEkhi/lead-knight
+cd lead-knight
+npm install
+sudo node .
